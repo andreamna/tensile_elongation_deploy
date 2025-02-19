13f75bac68cb0692cda1d3d5a18d103bbd488e5f
Andrea Mena Soriano <andrea1215ms@gmail.com>
2025-02-19 11:50:45 +0900

    kam images

--- backend/app.py
+++ backend/app.py
@@ -24,13 +24,18 @@ def find_closest_images(percentage, image_type):
         closest_upper = min([p for p in predefined_percentages if p >= percentage])
 
     folder = PHASE_MAP_IMG_FOLDER if image_type == "phase_map" else KAM_IMG_FOLDER
-    img_lower_path = os.path.join(folder, f"phase_map_{closest_lower}.png")
-    img_upper_path = os.path.join(folder, f"phase_map_{closest_upper}.png")
+
+    if image_type == "phase_map":
+        img_lower_path = os.path.join(folder, f"phase_map_{closest_lower}.png")
+        img_upper_path = os.path.join(folder, f"phase_map_{closest_upper}.png")
+    else:  
+        img_lower_path = os.path.join(folder, f"KAM_image_{closest_lower}.png")
+        img_upper_path = os.path.join(folder, f"KAM_image_{closest_upper}.png")
 
     return img_lower_path, img_upper_path, closest_lower, closest_upper
 
+
 def generate_morphed_image(percentage, image_type):
-    """Blends the two closest images to create an intermediate image."""
     img_lower_path, img_upper_path, lower_perc, upper_perc = find_closest_images(percentage, image_type)
 
     if not os.path.exists(img_lower_path) or not os.path.exists(img_upper_path):
@@ -62,20 +67,20 @@ def generate_image():
         if percentage > 60:
             return jsonify({"error": "Enter maximum 60%"}), 400
 
-        print(f"Processing request for {percentage}% ({image_type})")  # Debug Log
+        print(f"Processing request for {percentage}% ({image_type})")  
 
         image_path = generate_morphed_image(percentage, image_type)
 
         if image_path is None:
             return jsonify({"error": "Could not generate image"}), 500
 
-        print(f"Generated image at {image_path}")  # Debug Log
+        print(f"Generated image at {image_path}")  
 
         return send_file(image_path, mimetype="image/png", as_attachment=True, download_name=f"elongation_{percentage}.png")
 
     except Exception as e:
-        print(f"❌ ERROR: {e}")  # Debug Log
-        traceback.print_exc()  # Print full error details
+        print(f"❌ ERROR: {e}")  
+        traceback.print_exc()  
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
